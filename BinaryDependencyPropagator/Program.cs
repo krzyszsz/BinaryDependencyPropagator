a17404111843c10ccb43dad7e90305ae1d512135
Krzysztof Janiszewski <krzysztof.andrzej.janiszewski@gmail.com>
2019-03-16 04:22:12 +0000

    Continuation

--- BinaryDependencyPropagator/Program.cs
+++ BinaryDependencyPropagator/Program.cs
@@ -8,12 +8,37 @@ using System.Threading.Tasks;
 
 namespace BinaryDependencyPropagator
 {
+    /// <summary>
+    /// What it is:
+    /// This tool is meant to copy dll files between projects to facilitate debugging of .net applications.
+    /// It is useful when a project is composed of separate solutions requiring manual copying of binaries between the directories.
+    /// For example, many projects publish their binaries into a private NuGet repository and at a later stage of the build process
+    /// they download these binary dependencies in a different project.
+    /// To make debugging easier, you can locally use this tool to override your dll-s with the other dll-s you build on your machine
+    /// but including your own debugging symbols. This tool automates this process with the assumption that the dll-s with the newest
+    /// modification date should override the older dll-s with the same name.
+    /// 
+    /// Usage:
+    /// Modify the directory names in the main method to match your project directories.
+    /// Run it every time after you build one of the projects of your application that you would like to propagate
+    /// to the other parts of your project.
+    /// 
+    /// WARNING:
+    /// Think twice if this tool is the right choice in your case. It overrides *.dll files so YOU WILL LOSE some of your *.dll-s as
+    /// they will be overridden by the newer versions of these dll-s
+    /// 
+    /// WARNING-2:
+    /// This tool has not been extensively tested. It is a prototype and not a production-ready solution.
+    /// 
+    /// Think of it as a template for building your own solution of your own problem.
+    /// It is meant to be simple to modify so it looks more like a single-file script rather than a normal application.
+    /// </summary>
     class Program
     {
         public const string FileExtension = ".dll";
         public const string FileExtensionSearchPattern = "*.dll";
         public const string DebugSymbolsExtension = ".pdb";
-        public const string NugetDirectory = "/packages/";
+        public const string NugetDirectory = "/packages/";  // App excludes packages from source directories (as the point is to override packages and not the rest of dll-s)
 
         static void Main()
         {
@@ -21,8 +46,9 @@ namespace BinaryDependencyPropagator
             sw.Start();
             var filesToLookInto = new FileSearch().GetFiles(new List<FileSearchCriteria>
             {
-                new FileSearchCriteria { Root = @"c:\abc\cde" },
-                new FileSearchCriteria { Root = @"c:\abc\xyz" }
+                new FileSearchCriteria { Root = @"c:\abc\cde" },    // Please specify all your source directories here.
+                new FileSearchCriteria { Root = @"c:\abc\xyz" },
+                new FileSearchCriteria { Root = @"c:\abc\aabbb" },
             });
 
             new FileSystem().CopyNewerFilesWithPdb(filesToLookInto);
@@ -73,7 +99,7 @@ namespace BinaryDependencyPropagator
             var allFilesGroupedByName = files.ToLookup(x => Path.GetFileName(x.FullName));
 
             var subsetNotFromNuget = files.Where(x => !x.FullName.ToLower().Replace("\\", "/").Contains(Program.NugetDirectory));
-            var subsetWithPdb = subsetNotFromNuget.Where(x => x.FullName.ToLower().EndsWith(Program.FileExtension) && File.Exists(ToPdb(x.FullName))).ToList();
+            var subsetWithPdb = subsetNotFromNuget.AsParallel().Where(x => x.FullName.ToLower().EndsWith(Program.FileExtension) && File.Exists(ToPdb(x.FullName))).ToList();
             var srcCollection = subsetWithPdb.GroupBy(x => Path.GetFileName(x.FullName)).Select(x => new {itself = x, newest = x.Max(y => y.Date)}).Select(x => x.itself.First(y => y.Date == x.newest)).ToList();
             int processedFiles = 0;
             Parallel.ForEach(srcCollection, srcFile =>
